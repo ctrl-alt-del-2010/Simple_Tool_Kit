df2d9801ac1a275e07cbca41f17772ab9a077e08
ctrl-alt-del-2010 <amongus.iso2013@gmail.com>
2025-08-01 17:51:39 +0300

    Update setup.sh

--- start/setup.sh
+++ start/setup.sh
@@ -1,10 +1,13 @@
-sudo apt install torbrowser-launcher
-sudo apt install xterm
-sudo apt install php
-sudo apt install python3
-sudo apt install iwd
-sudo apt install tor
-
+sudo apt install torbrowser-launcher || echo "torbrowser-launcher is not installed."
 clear
-
+sudo apt install xterm || echo "xterm is not installed."
+clear
+sudo apt install php || echo "php is not installed."
+clear
+sudo apt install python3 || echo "python3 is not installed."
+clear
+sudo apt install iwd || echo "iwd is not installed."
+clear
+sudo apt install tor || echo "tor is not installed."
+clear
 echo 'Installation completed. use "./run.sh" to start the program.'
